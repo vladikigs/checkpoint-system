cea97f701bd592f4fdb2fce7eb8ac17f82fb8662
vlad <vladikigs@mail.ru>
2021-05-05 15:05:01 +0400

    Initial commit

---
+++ src/main/java/com/vladislav/bashirov/checkpoint/system/CheckpointSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vladislav.bashirov.checkpoint.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladislav/bashirov/checkpoint/system/CheckpointSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladislav.bashirov.checkpoint.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
